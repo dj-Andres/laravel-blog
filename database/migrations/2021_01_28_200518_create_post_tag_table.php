7da3c04778fd1715a2d3d6c216aea53d745bfe56
dj-Andres <andres96jimenez@gmail.com>
2021-11-20 16:06:25 -0500

    Procfile added

--- database/migrations/2021_01_28_200518_create_post_tag_table.php
+++ database/migrations/2021_01_28_200518_create_post_tag_table.php
@@ -17,7 +17,6 @@ class CreatePostTagTable extends Migration
             $table->id();
             $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('tag_id');
-
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             $table->timestamps();
